ac74f7b25adcb79e87190b5af8013358435cfca3
Siddharth More <simore@microsoft.com>
2024-03-03 19:00:25 -0800

    changes

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ import joblib
 app = Flask(__name__)
 
 # Load the pre-trained models (ensure the models are in the same directory as this script)
-modelA = joblib.load("diabetes_modelA.pkl")
-modelB = joblib.load("diabetes_modelB.pkl")
+model_a = load_model('model/diabetes_model1.h5')
+scaler_a = joblib.load('model/scaler1.pkl')
+
+model_b = load_model('model/diabetes_model2.h5')
+scaler_b = joblib.load('model/scaler2.pkl')
 
 models = {
-    'modelA': modelA,
-    'modelB': modelB
+    'a': model_a,
+    'b': model_b
+}
+
+scalers = {
+    'a': scaler_a,
+    'b': scaler_b
 }
 
 @app.route('/predict', methods=['POST'])
@@ -18,49 +26,21 @@ def predict():
     
     # Extract features and model choice from the request
     features = [data['features']]
-    model_name = data.get('model_name', 'modelA')  # Default to modelA if not specified
+    model_name = data.get('model_name', 'a')  # Default to model1 if not specified
     
     # Select the model based on the input parameter
     if model_name in models:
         selected_model = models[model_name]
+        selected_scaler = scalers[model_name]
     else:
         return jsonify({'error': 'Model not found'}), 404
     
     # Make prediction
-    prediction = selected_model.predict(features)
-    return jsonify({'prediction': int(prediction[0])})
+    features_scaled = selected_scaler.transform([features])
+    prediction = selected_model.predict(features_scaled)
+    prediction_label = (prediction > 0.5).astype(int)
+
+    return jsonify({'prediction': prediction.tolist(), 'prediction_label': prediction_label.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
-
-
-
-
-# from flask import Flask, request, jsonify
-# from tensorflow.keras.models import load_model
-# import numpy as np
-# import joblib
-
-# app = Flask(__name__)
-
-# # Load the model and scaler
-# model = load_model('model/diabetes_model.h5')
-# scaler = joblib.load('model/scaler.pkl')
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.get_json(force=True)
-#     features = np.array(data['features'])
-    
-#     # Scale the features
-#     features_scaled = scaler.transform([features])
-    
-#     # Make prediction
-#     prediction = model.predict(features_scaled)
-#     prediction_label = (prediction > 0.5).astype(int)
-    
-#     return jsonify({'prediction': prediction.tolist(), 'prediction_label': prediction_label.tolist()})
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
-
